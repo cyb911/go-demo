16948f47392e2c23097771a968ed5f0cabef916d
yunbin.chen <yunbin110@gmail.com>
2025-10-28 13:16:58 +0800

    1

--- task/01/one/main.go
+++ task/01/one/main.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
+/*
+The number that appears only once.
+*/
 func main() {
-	// 声明一个数组
 	nums := []int{4, 2, 4, 3, 0, 0, 3, 9, 9}
 
-	// 使用map进行技术
 	countMap := make(map[int]int)
 
 	for _, num := range nums {
 		countMap[num]++
 	}
 
-	//找出map，次数=1的key
 	for num, count := range countMap {
 		if count == 1 {
 			fmt.Println("num:", num, "count:", count)
