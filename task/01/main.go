d77a67a697a13a4f6b168d6420ed3e47615cf020
yunbin.chen <yunbin110@gmail.com>
2025-10-31 09:15:57 +0800

    最长公共前缀

--- task/01/main.go
+++ task/01/main.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{4, 2, 4, 3, 0, 0, 3, 9, 9}
-	fmt.Println(findAppearsOnce(nums))
-
-	fmt.Println(isPalindrome(121))
-	fmt.Println(isPalindrome(-121))
-	fmt.Println(isPalindrome(10))
-	fmt.Println(isPalindrome(12321))
+	//nums := []int{4, 2, 4, 3, 0, 0, 3, 9, 9}
+	//fmt.Println(findAppearsOnce(nums))
+	//
+	//fmt.Println(isPalindrome(121))
+	//fmt.Println(isPalindrome(-121))
+	//fmt.Println(isPalindrome(10))
+	//fmt.Println(isPalindrome(12321))
+	strs1 := []string{"flower", "flow", "flight"}
+	strs2 := []string{"com-dog", "com-racecar", "com-car"}
+	fmt.Println(longestCommonPrefix(strs1))
+	fmt.Println(longestCommonPrefix(strs2))
 }
 
 /*
@@ -51,4 +55,42 @@ func isPalindrome(x int) bool {
 	}
 
 	return x == b
+}
+
+/*
+最长公共前缀
+Case1: ["flower","flow","flight"]
+Case2: ["dog","racecar","car"]
+*/
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		prefix = commonPrefix(prefix, strs[i])
+		if prefix == "" { // 任意两个字符之间不存公共前缀，则任务整个集合中的元素不存在公共前缀。
+			return ""
+		}
+	}
+	return prefix
+}
+
+/*
+两个字符串中的公共前缀（适用于 ASCII）
+*/
+func commonPrefix(a, b string) string {
+	// 字符串进行字节数组元素比较
+	minLen := len(a)
+	if len(b) < minLen {
+		minLen = len(b)
+	}
+
+	for i := 0; i < minLen; i++ {
+		if a[i] != b[i] {
+			return a[:i]
+		}
+	}
+	return a[:minLen]
 }
